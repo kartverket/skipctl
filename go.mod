c6b82dcb065c37e5ca2d37c9bac8f07129f09611
Julian Grande <julian.grande@bekk.no>
2025-08-29 15:24:16 +0200

    feat: add `yaml` validation to `skipctl manifest validate` cmd

--- go.mod
+++ go.mod
@@ -15,7 +15,8 @@ require (
 	golang.org/x/oauth2 v0.30.0
 	google.golang.org/api v0.248.0
 	google.golang.org/grpc v1.75.0
-	google.golang.org/protobuf v1.36.8
+	google.golang.org/protobuf v1.36.7
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -122,7 +123,6 @@ require (
 	golang.org/x/tools v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250728155136-f173205681a0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250818200422-3122310a409c // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.2 // indirect
 	pluginrpc.com/pluginrpc v0.5.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
---
+++ pkg/manifest/validator.go
@@ -0,0 +1,47 @@
+package manifest
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-jsonnet"
+	"gopkg.in/yaml.v3"
+
+	"github.com/kartverket/skipctl/pkg/constants"
+)
+
+type Validator struct {
+	jsonnet *jsonnet.VM
+}
+
+func NewValidator() *Validator {
+	return &Validator{
+		jsonnet: jsonnet.MakeVM(),
+	}
+}
+
+func (v *Validator) ValidateManifest(filename string) error {
+	extension := filepath.Ext(filename)
+
+	switch extension { //nolint:gocritic // singleCaseSwitch: this is intentional
+	case constants.ManifestSuffixJsonnet:
+		return v.validateJsonnet(filename)
+	}
+	return nil
+}
+
+func (v *Validator) validateJsonnet(filename string) error {
+	_, err := v.jsonnet.EvaluateFile(filename)
+	return err
+}
+
+func (v *Validator) validateYaml(filename string) error {
+	fileContents, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	var output any
+
+	return yaml.Unmarshal(fileContents, &output)
+}
