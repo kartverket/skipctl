c440ceb6f15d2e94fcadd738ca272d8e99e75333
Andreas Espelund <andreas.espelund@bekk.no>
2025-09-02 08:22:12 +0200

    fix error

--- cmd/util.go
+++ cmd/util.go
@@ -40,7 +40,7 @@ func ValidateAPIServerName(_ *cobra.Command, _ []string) {
 
 func findFilesWithSuffixes(directory string, suffixes []string) ([]string, error) {
 	var files []string
-	err := filepath.WalkDir(directory, func(path string, info fs.DirEntry, err error) error {
+	err := filepath.WalkDir(directory, func(path string, info os.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
