071f24c9b129e545c8555b239ec71751ea22f1a6
Even Holthe <even.holthe@kartverket.no>
2025-09-07 00:15:42 +0200

    version: add supported schemas

--- pkg/crd/crd.go
+++ pkg/crd/crd.go
@@ -6,3 +6,19 @@ import "embed"
 //
 //go:embed schemas/*.json
 var Schemas embed.FS
+
+// ListSchemas lists the names of all embedded schema files.
+func ListSchemas() ([]string, error) {
+	entries, err := Schemas.ReadDir("schemas")
+	if err != nil {
+		return nil, err
+	}
+
+	var schemaFiles []string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			schemaFiles = append(schemaFiles, entry.Name())
+		}
+	}
+	return schemaFiles, nil
+}
