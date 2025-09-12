5aaaf479c0c8727da6eba9ec1804ac325fb24b17
Even Holthe <even.holthe@kartverket.no>
2025-09-12 12:27:06 +0200

    schemas: add ztoperator authpolicy

--- pkg/crd/schemagen.go
+++ pkg/crd/schemagen.go
@@ -26,3 +26,5 @@ package crd
 //go:generate crdjson -outdir=./schemas -one-per-kind -url=https://github.com/external-secrets/external-secrets/releases/download/v0.19.2/external-secrets.yaml
 
 //go:generate crdjson -outdir=./schemas -url=https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.27/releases/cnpg-1.27.0.yaml
+
+//go:generate crdjson -outdir=./schemas -url=https://raw.githubusercontent.com/kartverket/ztoperator/refs/heads/main/config/crd/bases/ztoperator.kartverket.no_authpolicies.yaml
